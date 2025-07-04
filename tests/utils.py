5fa0c5b37e0d50d60b836b4aa23ecef3c4be5f82
ifl <53334993+ivolabbe@users.noreply.github.com>
2025-07-05 02:08:43 +1000

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -66,7 +66,7 @@ def make_simple_data(
         g_fwhm_y = rng.uniform(2.0, 4.0)
         theta = rng.uniform(0, np.pi)
         g_size = 13
-        gauss = _gaussian_psf(g_size, g_fwhm_x, g_fwhm_y, theta)
+        gauss = PSF.gaussian(g_size, g_fwhm_x, g_fwhm_y, theta)
         r = g_size // 2
         yy = slice(y - r, y + r + 1)
         xx = slice(x - r, x + r + 1)

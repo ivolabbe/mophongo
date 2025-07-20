9db2472805f38878ccd218e99362aec611513527
ifl <53334993+ivolabbe@users.noreply.github.com>
2025-07-20 20:55:36 +1000

    Revert PSF map defaults and set global fwhm

--- src/mophongo/__init__.py
+++ src/mophongo/__init__.py
@@ -4,6 +4,9 @@ from .catalog import Catalog
 from .deblender import deblend_sources_symmetry, deblend_sources_hybrid
 from .jwst_psf import make_extended_grid
 from .psf_map import PSFRegionMap
+from . import psf_map as _psf_map
+
+_psf_map.fwhm = 0.2 / 3600
 
 try:
     from .photutils_deblend import deblend_sources
